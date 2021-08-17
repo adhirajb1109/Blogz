8fa482bc3a924db8ef439a846fce8c3a42157357
Adhiraj Bhatia <adhirajb1109@gmail.com>
2021-08-17 14:43:52 +0530

    commit

--- blogz/settings.py
+++ blogz/settings.py
@@ -117,7 +117,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+ALLOWED_HOSTS = "blogz-blogging.herokuapp.com"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
